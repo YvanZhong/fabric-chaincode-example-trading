39a5027ee9bdecdcaf965378da23427ef12980e6
Zhong <893646331@qq.com>
2019-01-04 19:11:40 +0800

    init

--- src/main/java/com/example/chaincode/TradingChaincode.java
+++ src/main/java/com/example/chaincode/TradingChaincode.java
@@ -18,13 +18,7 @@ public class TradingChaincode extends ChaincodeBase {
 
     private static final Charset CHARSET = StandardCharsets.UTF_8;
 
-    private static final Double DEFAULT_ACCOUNT_BALANCE = 0.0;
-
     private static final String KEY_METADATA = "METADATA";
-
-    private static final String KEY_PREFIX_CUSTOMER_ACCOUNT = "CUSTOMER_ACCOUNT_";
-
-    private static final String KEY_PREFIX_ACCOUNT_TRANSACTION = "ACCOUNT_TRANSACTION_";
 
     /**
      * 初始化
@@ -34,15 +28,15 @@ public class TradingChaincode extends ChaincodeBase {
         try {
             String func = stub.getFunction();
             if (!func.equals("init")) {
-                return newErrorResponse("函数错误!");
+                return newErrorResponse("function error!");
             }
             List<String> args = stub.getParameters();
             if (args.size() != 1) {
-                newErrorResponse("参数错误！");
+                newErrorResponse("params error!");
             }
-            LOGGER.info("初始化智能合约开始，function = {}, args = {}", func, args);
+            LOGGER.info("init begin, function = {}, args = {}", func, args);
             stub.putStringState(KEY_METADATA, args.get(0));
-            return newSuccessResponse("初始化智能合约成功!");
+            return newSuccessResponse("init success.");
         } catch (Throwable e) {
             return newErrorResponse(e);
         }
@@ -56,10 +50,10 @@ public class TradingChaincode extends ChaincodeBase {
         try {
             String function = stub.getFunction();
             List<String> args = stub.getParameters();
-            LOGGER.info("调用智能合约开始，function = {}, args = {}", function, args);
+            LOGGER.info("invoke begin, function = {}, args = {}", function, args);
             Response response = null;
             response = doInvoke(stub, function, args);
-            LOGGER.info("<<< 调用智能合约结束，response = [status = {}, message = {}, payload = {}]", response.getStatus().getCode(), response.getMessage(), response.getPayload() == null ? null : new String(response.getPayload(), CHARSET));
+            LOGGER.info("invoke success，response = [status = {}, message = {}, payload = {}]", response.getStatus().getCode(), response.getMessage(), response.getPayload() == null ? null : new String(response.getPayload(), CHARSET));
             return response;
         } catch (Throwable e) {
             LOGGER.error(e.getMessage(), e);
